db87771cb576ab9541a492550864c99287f03099
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-07-11 21:06:51 +0200

    :construction: Utilise le cache pour récupérer les ressources

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+const CURRENT_CACHE_NAME = 'v1';
+
 self.addEventListener('install', event => {
   console.log('⤵️ Installation du Service Worker...');
 });
@@ -8,4 +10,26 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   console.log('🕸 Interception d\'un fetch vers :', event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+      if (cachedResponse) {
+        console.log('💿 Réponse depuis le cache pour :', event.request.url);
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+        caches.open(CURRENT_CACHE_NAME).then(cache => {
+          console.log('📲 Mise en cache de :', event.request.url);
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
 });
